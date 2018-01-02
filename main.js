7964e839f757e77f799f4bc8e62882c7a6543dbc
anattzlil <anattzlil@gmail.com>
2018-01-02 11:48:04 +0200

    list of 10 books

--- main.js
+++ main.js
@@ -1,36 +1,46 @@
-var fetch = function (isbn) {
+var fetch = function (bookTitle) {
     $.ajax({
       method: "GET",
-      url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn,
+      url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + bookTitle,
       success: function(data) {
-        var title = data.items[0].volumeInfo.title;
-        var author = data.items[0].volumeInfo.authors;
-        var description = data.items[0].volumeInfo.description;
-        var img = data.items[0].volumeInfo.imageLinks.thumbnail;
         console.log(data);
-        renderBook(title, description, author, img);
+        // createBookArray(data);
+        renderBook(data);
+        // console.log(bookList);
       },
       error: function(jqXHR, textStatus, errorThrown) {
         console.log(textStatus);
       }
     }); 
   };
+// var bookList = {
+//   bookArray: []
+// }
+// var createBookArray = function(data){
+//   for (var i =0; i < 10; i++){
+//     var title = data.items[i].volumeInfo.title;
+//     var author = data.items[i].volumeInfo.authors;
+//     var description = data.items[i].volumeInfo.description;
+//     var img = data.items[i].volumeInfo.imageLinks.thumbnail;
+//     var chosenBook = {
+//       bookTitle: title,
+//       bookDescription: description,
+//       bookAuthor: author,
+//       bookImg:img
+//     }; 
+//     bookList.bookArray.push(chosenBook);
+//   }
+// }
 
-var renderBook = function(title, description, author, img){
-  var chosenBook = {
-    title: title,
-    description: description,
-    author: author,
-    img:img
-  };  
+var renderBook = function(data){ 
     var source = $('#books-template').html();
     var template = Handlebars.compile(source);
-    var newHTML = template(chosenBook); 
+    var newHTML = template(data); 
     $('.bookList').append(newHTML);
 }
   $('.fetch').on('click', function(){
-    var isbn = $(this).siblings('.input').val();   
-    fetch(isbn);
+    var bookTitle = $(this).siblings('.input').val();   
+    fetch(bookTitle);
     $(this).siblings('.input').val(""); 
   });
 
